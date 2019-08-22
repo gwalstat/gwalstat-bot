2fd0164f02ca120eb7c8e7b9cc93782b78d65d1c
krnick <sungboss2004@gmail.com>
2019-08-22 15:15:39 +0800

    adjust the order before clone

--- gwalstat/git_util.py
+++ gwalstat/git_util.py
@@ -6,16 +6,12 @@ import shutil
 def get_branch(url, br):
 
     dirpath = tempfile.mkdtemp()
-    repo = git.Repo.clone_from(url=url, to_path=dirpath, branch=br)
-
-    origin = repo.remotes.origin
     git_command = git.cmd.Git()
-
     matching = []
 
     while len(matching) == 0:
         # Checking the remote ref exists or not.
-        print("Test the remote branch")
+        print("Testing the remote branch")
         list_ref = git_command.ls_remote(url).split("\n")
         matching = [s for s in list_ref if br in s]
 
@@ -23,6 +19,8 @@ def get_branch(url, br):
     del git_command
 
     print("Start to fetching")
+
+    repo = git.Repo.clone_from(url=url, to_path=dirpath, branch=br)
 
     return dirpath
 
